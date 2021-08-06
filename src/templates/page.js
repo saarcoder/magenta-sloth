55bca5b538dec737a9c09689bebee6773586c7fb
Stackbit Code Editor <projects@stackbit.com>
2021-08-06 08:33:19 +0000

    page.js: changed

--- src/templates/page.js
+++ src/templates/page.js
@@ -30,7 +30,6 @@ export default class Page extends React.Component {
             	</div>
             </article>
             </Layout>
-            <iframe src="https://www.mcc-berlin.net/fileadmin/data/clock/carbon_clock.htm?i=3267263" style="width:600px; height:340px;"></iframe>
         );
     }
 }
